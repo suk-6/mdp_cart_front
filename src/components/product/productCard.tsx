b54ce7182c9802d24f5f3251e2c99d739bc6d896
suk-6 <me@suk.kr>
2024-06-20 21:37:58 +0900

    feat: Add ProductCard component for displaying product information and adding to cart

---
+++ src/components/product/productCard.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Product } from "@/models/product";
+
+interface ProductCardProps {
+	product: Product;
+	onAddToCart: (product: Product) => void;
+}
+
+const formatter = new Intl.NumberFormat("ko-KR").format;
+
+export const ProductCard = ({ product, onAddToCart }: ProductCardProps) => {
+	return (
+		<div className=" w-full h-32 bg-gray-100 rounded-lg shadow-lg">
+			<div className=" flex flex-col gap-2 p-4">
+				<div className=" text-lg font-bold">{product.name}</div>
+				<div className=" text-sm">₩{formatter(product.price)}원</div>
+				<button
+					onClick={() => onAddToCart(product)}
+					className=" bg-[#ff6b6b] hover:bg-[#ff4b4b] text-white rounded-lg py-1"
+				>
+					장바구니에 담기
+				</button>
+			</div>
+		</div>
+	);
+};
