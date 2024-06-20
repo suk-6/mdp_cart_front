ec240d9f67f16d340f1d24e76564473c315179af
suk-6 <me@suk.kr>
2024-06-20 21:17:12 +0900

    feat: Update header component to include navigation links

--- src/components/semantic/header.tsx
+++ src/components/semantic/header.tsx
@@ -1,14 +1,30 @@
+import Link from "next/link";
+
 export const Header = () => {
 	return (
 		<header className="w-full h-16 bg-[#333] text-white flex items-center justify-between p-4">
-			<h1 className="text-2xl">🛒통통카트</h1>
+			<Link href={"/"}>
+				<h1 className="text-2xl">🛒통통카트</h1>
+			</Link>
 			<nav className=" flex flex-row gap-3">
-				<div>홈</div>
-				<div>제품</div>
-				<div>소개</div>
-				<div>문의</div>
-				<div>로그인</div>
-				<div>회원가입</div>
+				<Link href={"/"}>
+					<div>홈</div>
+				</Link>
+				<Link href={"/product"}>
+					<div>제품</div>
+				</Link>
+				<Link href={"/about"}>
+					<div>소개</div>
+				</Link>
+				<Link href={"/contact"}>
+					<div>문의</div>
+				</Link>
+				<Link href={"/login"}>
+					<div>로그인</div>
+				</Link>
+				<Link href={"/register"}>
+					<div>회원가입</div>
+				</Link>
 			</nav>
 		</header>
 	);
