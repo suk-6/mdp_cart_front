4a0d2d9237871b33dcb3bb0d6bf03b8c2cc725d3
suk-6 <me@suk.kr>
2024-06-20 21:14:15 +0900

    chore: Update layout component to include header and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Header } from "@/components/semantic/header";
+import { Footer } from "@/components/semantic/footer";
 
 export const metadata: Metadata = {
 	title: "🛒통통카트",
@@ -16,7 +15,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="ko">
-			<body className={inter.className}>{children}</body>
+			<body>
+				<Header />
+				{children}
+				<Footer />
+			</body>
 		</html>
 	);
 }
