62f8d8521132059cfb2162c55c842bfa0887a494
suk-6 <me@suk.kr>
2024-06-20 21:14:11 +0900

    feat: Add footer component

---
+++ src/components/semantic/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+	return (
+		<footer className="w-full h-16 bg-[#333] text-white flex items-center justify-center p-4">
+			<div>© 2024 통통카트. 모든 권리 보유.</div>
+		</footer>
+	);
+};
