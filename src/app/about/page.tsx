6cd78792c82ddcaadbc758679b17731ff2af7ffe
suk-6 <me@suk.kr>
2024-06-20 21:37:42 +0900

    feat: Add About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function AboutPage() {
+	return (
+		<div className=" w-full h-fit flex flex-col gap-0 pt-10">
+			<div className=" text-center font-bold text-2xl">
+				통통카트에 대하여
+			</div>
+			<div className=" flex flex-col gap-2 px-14 py-6">
+				<div>
+					통통카트는 최고 품질의 제품을 최고의 가격으로 제공합니다.
+				</div>
+				<div>
+					우리의 목표는 고객에게 최상의 쇼핑 경험을 제공하는 것입니다.
+				</div>
+			</div>
+		</div>
+	);
+}
