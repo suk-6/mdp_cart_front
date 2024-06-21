2f9b9be03298aa046ad37791f0a6e99c4b0e1860
suk-6 <me@suk.kr>
2024-06-21 10:18:11 +0900

    feat: Update product page to dynamically render product cards

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,28 +1,20 @@
 "use client";
 
 import { ProductCard } from "@/components/product/productCard";
+import data from "@/data.json";
 
 export default function ProductPage() {
 	return (
 		<div className=" w-full h-fit flex flex-col gap-6 pt-10">
 			<div className="text-center text-3xl font-bold">제품 목록</div>
 			<div className=" flex flex-col p-10 gap-6">
-				<ProductCard
-					product={{ id: 1, name: "제품1", price: 10000 }}
-					onAddToCart={() => {}}
-				/>
-				<ProductCard
-					product={{ id: 2, name: "제품2", price: 20000 }}
-					onAddToCart={() => {}}
-				/>
-				<ProductCard
-					product={{ id: 3, name: "제품3", price: 30000 }}
-					onAddToCart={() => {}}
-				/>
-				<ProductCard
-					product={{ id: 4, name: "제품4", price: 40000 }}
-					onAddToCart={() => {}}
-				/>
+				{data.products.map((product) => (
+					<ProductCard
+						key={product.id}
+						product={product}
+						onAddToCart={() => {}}
+					/>
+				))}
 			</div>
 		</div>
 	);
