c9584ebc71e48b276dad6aa55692d27a68cb2054
suk-6 <me@suk.kr>
2024-06-21 10:21:40 +0900

    chore: Update header component to include navigation links dynamically

--- src/components/semantic/header.tsx
+++ src/components/semantic/header.tsx
@@ -1,5 +1,14 @@
 import Link from "next/link";
 
+const pages = [
+	{ name: "홈", href: "/" },
+	{ name: "제품", href: "/product" },
+	{ name: "소개", href: "/about" },
+	{ name: "문의", href: "/contact" },
+	{ name: "로그인", href: "/login" },
+	{ name: "회원가입", href: "/register" },
+];
+
 export const Header = () => {
 	return (
 		<header className="w-full h-16 bg-[#333] text-white flex items-center justify-between p-6 px-10">
@@ -7,24 +16,11 @@ export const Header = () => {
 				<h1 className="text-3xl font-bold">🛒통통카트</h1>
 			</Link>
 			<nav className=" flex flex-row gap-3">
-				<Link href={"/"}>
-					<div>홈</div>
-				</Link>
-				<Link href={"/product"}>
-					<div>제품</div>
-				</Link>
-				<Link href={"/about"}>
-					<div>소개</div>
-				</Link>
-				<Link href={"/contact"}>
-					<div>문의</div>
-				</Link>
-				<Link href={"/login"}>
-					<div>로그인</div>
-				</Link>
-				<Link href={"/register"}>
-					<div>회원가입</div>
-				</Link>
+				{pages.map((page) => (
+					<Link key={page.href} href={page.href}>
+						<div>{page.name}</div>
+					</Link>
+				))}
 			</nav>
 		</header>
 	);
