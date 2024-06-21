c064597b2e1c53286c9d32bf6cba60f0f2fee1fa
suk-6 <me@suk.kr>
2024-06-21 11:06:08 +0900

    feat: Update ProductPage with centered layout and responsive design

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -5,9 +5,9 @@ import data from "@/data.json";
 
 export default function ProductPage() {
 	return (
-		<div className=" w-full h-fit flex flex-col gap-6 pt-10">
+		<div className=" w-full h-fit flex flex-col items-center gap-6 pt-10 ">
 			<div className="text-center text-3xl font-bold">제품 목록</div>
-			<div className=" flex flex-col p-10 gap-6">
+			<div className=" w-full flex flex-wrap gap-6 justify-center items-center p-20">
 				{data.products.map((product) => (
 					<ProductCard
 						key={product.id}
