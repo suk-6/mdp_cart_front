234ba222526e9349fe03d1ee463a503215cd69dc
suk-6 <me@suk.kr>
2024-06-21 10:42:07 +0900

    feat: Create RegisterPage component

---
+++ src/app/register/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+export default function RegisterPage() {
+	const register = () => {
+		const id = document.getElementById("id") as HTMLInputElement;
+		const password = document.getElementById(
+			"password"
+		) as HTMLInputElement;
+		const email = document.getElementById("email") as HTMLInputElement;
+		const name = document.getElementById("name") as HTMLInputElement;
+
+		if (
+			id.value === "" ||
+			password.value === "" ||
+			email.value === "" ||
+			name.value === ""
+		) {
+			alert("모든 항목을 입력해주세요");
+			return;
+		}
+
+		alert("회원가입 성공");
+	};
+
+	return (
+		<div className=" w-full h-fit flex flex-col pt-10">
+			<div className=" flex justify-center items-center w-full h-full p-10 my-20">
+				<div className=" p-36 flex flex-col">
+					<div className=" text-center font-bold text-xl">Login</div>
+					<div className=" w-96 flex flex-col gap-4 mt-10">
+						<input
+							type="text"
+							id="id"
+							name="id"
+							placeholder="ID"
+							className=" p-2 border border-gray-300 rounded-md"
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									register();
+								}
+							}}
+						/>
+						<input
+							type="password"
+							id="password"
+							name="password"
+							placeholder="Password"
+							className=" p-2 border border-gray-300 rounded-md"
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									register();
+								}
+							}}
+						/>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							placeholder="이메일"
+							className=" p-2 border border-gray-300 rounded-md"
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									register();
+								}
+							}}
+						/>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							placeholder="이름"
+							className=" p-2 border border-gray-300 rounded-md"
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									register();
+								}
+							}}
+						/>
+						<button
+							className=" bg-[#ff6b6b] hover:bg-[#ff4b4b] text-white rounded-lg py-1"
+							onClick={register}
+						>
+							회원가입
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
