d480f2e7a945a61505b07b99c64c1e0d4aee72d0
suk-6 <me@suk.kr>
2024-06-23 20:12:36 +0900

    refactor: Update cartSlice to handle adding items to the cart with quantity controls

--- src/lib/features/cart/cartSlice.ts
+++ src/lib/features/cart/cartSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "@/lib/store";
-import { ProductInCart } from "@/models/product";
+import { Product, ProductInCart } from "@/models/product";
+import { Slide, toast } from "react-toastify";
 
 export interface CartState {
 	items: ProductInCart[];
@@ -17,8 +18,23 @@ export const cartSlice = createSlice({
 		initializeProduct: (state) => {
 			state.items = [];
 		},
-		addItem: (state, action: PayloadAction<ProductInCart>) => {
-			state.items.push(action.payload);
+		addItem: (state, action: PayloadAction<Product>) => {
+			if (state.items.find((item) => item.id === action.payload.id)) {
+				state.items = state.items.map((item) =>
+					item.id === action.payload.id
+						? { ...item, quantity: item.quantity + 1 }
+						: item
+				);
+			} else state.items.push({ ...action.payload, quantity: 1 });
+
+			toast.info("장바구니에 추가되었습니다.", {
+				position: "bottom-right",
+				autoClose: 3000,
+				hideProgressBar: true,
+				progress: 0,
+				theme: "light",
+				transition: Slide,
+			});
 		},
 		removeItem: (state, action: PayloadAction<number>) => {
 			state.items = state.items.filter(
