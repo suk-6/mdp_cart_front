b3402980365d0ff6b28cbbce2351d96d4641e785
suk-6 <me@suk.kr>
2024-06-24 11:47:11 +0900

    refactor: Update NextAuth configuration to use exported options

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,45 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/db/prisma";
+import bcrypt from "bcrypt";
+import { UserModel } from "@/models/user";
+import { AuthOptions } from "next-auth";
+
+export const authOptions: AuthOptions = {
+	pages: {
+		signIn: "login",
+	},
+	providers: [
+		CredentialsProvider({
+			name: "Login",
+			credentials: {
+				id: {
+					label: "ID",
+					type: "text",
+				},
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials, req): Promise<UserModel | null> {
+				if (!credentials) return null;
+
+				const user = await prisma.user.findUnique({
+					where: {
+						id: credentials.id,
+					},
+				});
+				if (!user) return null;
+
+				const passwordMatched = await bcrypt.compare(
+					credentials.password,
+					user.password!
+				);
+				if (!passwordMatched) return null;
+
+				return {
+					id: user.id,
+					email: user.email,
+					name: user.name,
+				};
+			},
+		}),
+	],
+};
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,5 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/db/prisma";
-import bcrypt from "bcrypt";
-import { UserModel } from "@/models/user";
-
-export const authOptions = {
-	pages: {
-		signIn: "login",
-	},
-	providers: [
-		CredentialsProvider({
-			name: "Login",
-			credentials: {
-				id: {
-					label: "ID",
-					type: "text",
-				},
-				password: { label: "Password", type: "password" },
-			},
-			async authorize(credentials, req): Promise<UserModel | null> {
-				if (!credentials) return null;
-
-				const user = await prisma.user.findUnique({
-					where: {
-						id: credentials.id,
-					},
-				});
-				if (!user) return null;
-
-				const passwordMatched = await bcrypt.compare(
-					credentials.password,
-					user.password!
-				);
-				if (!passwordMatched) return null;
-
-				return {
-					id: user.id,
-					email: user.email,
-					name: user.name,
-				};
-			},
-		}),
-	],
-};
+import { authOptions } from "./options";
 
 const handler = NextAuth(authOptions);
 
