ccc561fe41d6a394e9dd087568dbbf54ed008d63
suk-6 <me@suk.kr>
2024-06-24 09:18:13 +0900

    refactor: Update header component to dynamically render navigation links based on user authentication status

--- src/components/semantic/header.tsx
+++ src/components/semantic/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 const pages = [
@@ -6,11 +8,22 @@ const pages = [
 	{ name: "장바구니", href: "/cart" },
 	{ name: "소개", href: "/about" },
 	{ name: "문의", href: "/contact" },
+];
+
+const unauthenticatedPages = [
 	{ name: "로그인", href: "/login" },
 	{ name: "회원가입", href: "/register" },
 ];
 
+const authenticatedPages = [
+	{ name: "로그아웃", href: "/logout" },
+	{ name: "마이페이지", href: "/mypage" },
+];
+
 export const Header = () => {
+	const session = useSession();
+	const isAuthenticated = session.data;
+
 	return (
 		<header className="w-full h-16 bg-[#333] text-white flex items-center justify-between p-6 px-20 text-nowrap">
 			<Link href={"/"}>
@@ -22,6 +35,18 @@ export const Header = () => {
 						<div>{page.name}</div>
 					</Link>
 				))}
+				{isAuthenticated &&
+					authenticatedPages.map((page) => (
+						<Link key={page.href} href={page.href}>
+							<div>{page.name}</div>
+						</Link>
+					))}
+				{!isAuthenticated &&
+					unauthenticatedPages.map((page) => (
+						<Link key={page.href} href={page.href}>
+							<div>{page.name}</div>
+						</Link>
+					))}
 			</nav>
 		</header>
 	);
